c16787ddce060b124f39b763db4df7b32cc0337f
Mr Timo <hungryhungrytimo@gmail.com>
2012-09-05 21:28:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hyperapp::Application.config.secret_token = '6417a82ccfb9ef6bf7a38a88dbfbe93af2e8571c052c1ce4a85fd02083e31736923fc1ff77bf064fc673931264019241d22089b758ba77e8137d8f6a716f1556'
